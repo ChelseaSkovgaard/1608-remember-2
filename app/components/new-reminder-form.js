10d00efc6008f9207c6c6b787acdebcc3b409255
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-08 15:27:57 -0700

    Add a new reminder and save to store fully functional

--- app/components/new-reminder-form.js
+++ app/components/new-reminder-form.js
@@ -3,15 +3,16 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
-  name: '',
+  title: '',
   date: '',
   notes: '',
 
   actions: {
     saveNewReminder() {
-       const reminder = this.getProperties('name', 'date', 'notes');
+       const reminder = this.getProperties('title', 'date', 'notes');
+       reminder.date = new Date(reminder.date);
        this.get('store').createRecord('reminder', reminder).save().then(() => {
-         this.setProperties({name: '', date: '', notes: ''});
+         this.setProperties({title: '', date: '', notes: ''});
        }
      )
     }
