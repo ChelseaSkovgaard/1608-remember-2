b0e14b34b766ae2e06277c94ddb8e1e8acd49d6b
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-08 14:28:12 -0700

    WIP CreateRecord in store of new reminder. New item created in store, but values are not recorded.

--- app/components/save-reminder-button.js
+++ app/components/save-reminder-button.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  name: '',
+  date: '',
+  notes: '',
+
+  actions: {
+    saveNewReminder() {
+       const reminder = this.getProperties('name', 'date', 'notes');
+       this.get('store').createRecord('reminder', reminder).save().then(() => {
+         this.setProperties({name: '', date: '', notes: ''});
+       }
+     )
+    }
+  }
 });
