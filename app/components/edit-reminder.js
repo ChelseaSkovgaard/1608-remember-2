1de21955dd2adb154c91efbde8f9b09fe6701767
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-11 11:53:40 -0700

    WIP Saving edited reminder data, not pushing edits to store

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  title: '',
+  date: '',
+  notes: '',
+
+  actions: {
+    editReminder() {
+       const reminder = this.getProperties('title', 'date', 'notes');
+       reminder.date = new Date(reminder.date);
+       this.get('store').findRecord('reminder', 1).then(() => {
+         this.set('title','');
+         this.set('date', '');
+         this.set('notes', '');
+       });
+    }
+  }
 });
