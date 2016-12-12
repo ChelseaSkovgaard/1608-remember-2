834e0c255c2416617308691df9baa0a2955ffe37
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-12 15:21:23 -0700

    Save new and Edit actions both working for reminder form component

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -14,16 +14,16 @@ export default Ember.Component.extend({
        this.get('store').createRecord('reminder', reminder).save().then(() => {
          this.setProperties({title: '', date: '', notes: ''});
        });
-    }
-    editReminder(model) {
-      const edits = this.getProperties('title', 'date', 'notes');
-      edits.date = new Date(edits.date);
-       this.get('store').findRecord('reminder', params.reminder_id).then((edit) => {
-         edit.set('title', edits.title);
-         edit.set('date', edits.date);
-         edit.set('notes', edits.notes);
-        this.setProperties({title: '', date: '', notes: ''});
-       });
-    }
-  }
+    },
+    editReminder() {
+   const edits = this.getProperties('title', 'date', 'notes');
+   edits.date = new Date(edits.date);
+    this.get('store').findRecord('reminder', 1).then((edit) => {
+      edit.set('title', edits.title);
+      edit.set('date', edits.date);
+      edit.set('notes', edits.notes);
+     this.setProperties({title: '', date: '', notes: ''});
+    });
+ }
+}
 });
