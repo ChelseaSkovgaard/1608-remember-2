d24300812c34b7e5954a9457f407c46e7400c6f5
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-12-13 14:14:32 -0700

    delete functionality on individual page working

--- app/routes/reminders/reminder.js
+++ app/routes/reminders/reminder.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').find('reminder', params.reminder_id);
+  },
+  actions: {
+    deleteReminder(model) {
+      console.log(model.id);
+      model.destroyRecord('reminder', model.id);
+      this.transitionTo('reminders');
+    }
   }
 });
