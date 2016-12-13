71f197860657b86630f6a968afba7c970bf96eb9
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-13 15:20:34 -0700

    Add delete button to reminderS.hbs, is fully functional, has odd error

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.get('store').findAll('reminder');
+  },
+
+  actions: {
+    deleteReminder(model) {
+      this.get('store').findRecord('reminder',  model.id).then(() => {
+       model.destroyRecord('reminder', model.id);
+     });
+   }
   }
 });
