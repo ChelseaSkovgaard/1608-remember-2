f9fab272c7a9e8299af659863c4656d48a97442c
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-12 17:45:05 -0700

    Fix error on handling action when editing

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
-  title: '',
-  date: '',
-  notes: '',
-
   actions: {
     saveNewReminder(model) {
        model.date = model.date || new Date();
--- app/routes/reminders/edit.js
+++ app/routes/reminders/edit.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').find('reminder', params.reminder_id);
+  },
+  actions: {
+    saveNewReminder() {
+      this.transitionTo('reminders')
+    }
   }
 });
