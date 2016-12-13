037226ccf36821105ea4012e81388f5ef757a6e0
Kristen Burgess <kristen.burgess@doane.edu>
2016-12-12 17:52:18 -0700

    Update buttons for enhanced UX

--- app/components/reminder-form.js
+++ app/components/reminder-form.js
@@ -6,9 +6,8 @@ export default Ember.Component.extend({
   actions: {
     saveNewReminder(model) {
        model.date = model.date || new Date();
-       model.save().then(()=> {
-         this.sendAction()
-       })
+       model.save()
+      this.sendAction()
     }
   }
 });
